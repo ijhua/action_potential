8f1601c75254e5b2fb43f8f5b2d676e131d055c9
ijhua <hua.isabelle@gmail.com>
2018-02-01 17:44:54 -0800

    fixing issues with js file

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-<head>
-               <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-</head>
-
-<script>
-	TESTER = document.getElementById('tester');
-	Plotly.plot( TESTER, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
-  console.log( Plotly.BUILD );
-</script>
+TESTER = document.getElementById('tester');
+Plotly.plot( TESTER, [{
+x: [1, 2, 3, 4, 5],
+y: [1, 2, 4, 8, 16] }], {
+margin: { t: 0 } } );
+console.log( Plotly.BUILD );
